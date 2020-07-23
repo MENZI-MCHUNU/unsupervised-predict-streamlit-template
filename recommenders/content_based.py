15c4b8776d9a04a9f5fb98772e2d64781efd864f
Menzi Mchunu <menzi639@gmail.com>
2020-07-23 06:54:38 +0200

    debugging errors

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -157,9 +157,10 @@ def content_model(movie_list,top_n=10):
     count_vec = CountVectorizer()
     count_matrix = count_vec.fit_transform(data['bag_of_words'])
     indices = pd.Series(data['Title'])
+    st.write(indices)
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list.loc[0]]
+    idx_1 = indices[indices == movie_list[0]].loc[0]
     idx_2 = indices[indices == movie_list[1]].index[0]
     idx_3 = indices[indices == movie_list[2]].index[0]
     st.write(idx_1)

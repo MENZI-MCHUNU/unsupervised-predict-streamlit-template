641edc78bf429549f9ff22f236235162f871e226
Menzi Mchunu <menzi639@gmail.com>
2020-07-23 05:43:00 +0200

    debugging errors

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -180,5 +180,5 @@ def content_model(movie_list,top_n=10):
     # Removing chosen movies
     top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
     for i in top_indexes[:top_n]:
-        recommended_movies.append(list(data.index)[i])
+        recommended_movies.append(list(movies['title'])[i])
     return recommended_movies

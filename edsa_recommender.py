a5488ae134b08a5dd55a1320713821c681c2ceb1
Menzi Mchunu <menzi639@gmail.com>
2020-07-24 07:59:48 +0200

    fix TypeError

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -365,9 +365,10 @@ def main():
         #user_Id = rating_m['userId'].unique()
         #user_id1 = st.selectbox('Fisrt Option',user_Id[:1000])
         title_list1 = load_movie_titles('recommenders/hybrid_movies.csv')
+        title_list1 = title_list1.unique()
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list1[1:10].unique())
+        movie_1 = st.selectbox('Fisrt Option',title_list1[1:10])
 
         if st.button("Recommend"):
             #try:

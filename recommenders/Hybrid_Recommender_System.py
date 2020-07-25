e36d6fc9ad16166a52515778f490bc4f04026fc2
Menzi Mchunu <menzi639@gmail.com>
2020-07-25 12:19:07 +0200

    fix TypeError

--- recommenders/Hybrid_Recommender_System.py
+++ recommenders/Hybrid_Recommender_System.py
@@ -112,9 +112,9 @@ def recommendation(movie):
     result=[]
     #Getting the id of the movie for which the user want recommendation
     st.write(movie[0])
-    ind=indices[titles.loc[0] == movie[0]]
+    ind=indices[indices.loc[0] == movie[0]]
     #np.where(v == maximum)
-    #st.write(ind)
+    st.write(ind)
     #Getting all the similar cosine score for that movie
     sim_scores=list(enumerate(cosine_sim[ind]))
     #st.write(sim_scores)

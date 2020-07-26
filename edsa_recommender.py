a28d0dcc154e50d87fd38a8c9764a58c3c25dcf7
Menzi Mchunu <menzi639@gmail.com>
2020-07-26 06:51:29 +0200

    debugging code

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -355,7 +355,7 @@ def main():
                     st.write(top_recommendations)
                 st.title("We think you'll like:")
                 for i,j in enumerate(top_recommendations):
-                    #st.write(str(i+1),j)
+                    st.write(str(i+1),j)
                     st.subheader(str(i+1)+'. '+j)
 
     if page_selection == "Instruction of use":

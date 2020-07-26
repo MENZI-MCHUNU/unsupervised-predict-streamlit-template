59ad08352ce17e009d9550de2c7e71b9715b1b41
Menzi Mchunu <menzi639@gmail.com>
2020-07-26 09:25:18 +0200

    comment out debugging code

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -355,10 +355,10 @@ def main():
             #try:
                 with st.spinner('Crunching the numbers...'):
                     top_recommendations = recommendation(fav_movies, top_n = 10)
-                    st.write(top_recommendations)
+                    #st.write(top_recommendations)
                 st.title("We think you'll like:")
                 for i,j in enumerate(top_recommendations):
-                    st.write(str(i+1),j)
+                    #st.write(str(i+1),j)
                     st.subheader(str(i+1)+'. '+j)
 
     if page_selection == "Instruction of use":

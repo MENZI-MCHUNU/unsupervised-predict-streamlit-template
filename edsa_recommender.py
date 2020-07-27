93a93be3450881de4b0c76d93678121c449f512b
Menzi Mchunu <menzi639@gmail.com>
2020-07-27 15:18:42 +0200

    Adding an image as a link to netflix

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -405,7 +405,7 @@ def main():
         st.markdown("Plot : After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine-'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought. ")
         st.markdown("<a href='https://www.netflix.com/za/'><img src='resources/imgs/download.png'></a>", unsafe_allow_html=True)
         st.markdown("[<img src=resources/imgs/download.png>](https://www.netflix.com/za/)", unsafe_allow_html=True)#[![this is an image link](upload://7FxfXwDqJIZdYJ2QYADywvNRjB.png)](https://streamlit.io)
-        st.markdown('<img src="resources/imgs/download.png"/>', unsafe_allow_html=True)
+        st.markdown('[<img src="resources/imgs/download.png"/>](https://www.netflix.com/za/)', unsafe_allow_html=True)
         st.image('resources/imgs/download.png',width = 600)
     # Building out the About Machine Learning App page
     if page_selection == "About Machine Learning App":
